a2f2f05b537b51dbf91c756be769e0190745fff7
tiakia <1907325955@qq.com>
2018-01-12 17:38:40 +0800

    拖拽修复，点击省份放大显示

--- README.md
+++ README.md
@@ -3,4 +3,47 @@ d3.js做的中国地图
 ![d3Map](http://ostu98x74.bkt.clouddn.com/github/d3Map.png);
 ### 鼠标悬停显示 省份名称
 ### 点击城市，显示城市信息
+### 注意点：
+#### - 拖拽
+ - 如果是针对 `g` 组的话 因为它没有 `x`、`y` 所以使用 `translate`平移来实现拖拽效果。
+ - 这里需要获得俩个点，一个是在平移刚开始时候坐标位置，另一个是在平移的过程中的坐标位置
+ - 平移过程中 可以 通过 `d3.event.x` 来获得当前的坐标位置。
+ - 困住我的是起始点的位置。我刚开始是 在`dragstart`的时候，获得`d3.event.sourceEvent.offsetX` 来获取，然后 用当前 坐标位置 减去 起始点的位置
+ - 这个方案不成功，在拖拽第一个的是成功的，第二次拖拽的时候就会产生偏差
+ - 可能是在拖拽开始时候，产生了偏差
+ - 在 stackoverflow 搜到 可以在 鼠标按下的时候 就获取初始位置的 坐标
+ ```
+   g.attr('transform', "translate(0,0)")
+    .on('mousedown', function(){
+      startX = d3.mouse(this)[0];
+      startY = d3.mouse(this)[1];
+      console.log('mouseDown '+startX, startY);
+    })
+    .call(dragListener);
 
+ ```
+
+#### d3 双击事件 为 dblclick
+#### d3 清画布 d3.selectAll(selector).remove()
+这个是把选中的删除了，如果要移除子元素的 选择器 可以这么写 
+```
+d3.selectAll("selector > *").remove()
+```
+#### 根据经纬度 确定 城市的坐标 
+```
+ projection([d.properties.cp[0],d.properties.cp[1]])[0];
+```
+- projection 是 定义 的投影
+- d.properties.cp 是城市的经纬度 数组
+#### 优雅的数组降维
+```
+  var cityData = [];
+  //数组降维 获得 cityData
+  geoData.forEach(val =>{
+    if(!val.properties.cities) return;
+    cityData = cityData.concat(val.properties.cities);
+    return cityData;
+  });
+
+```
+- val.properties.cities 是一个数组 通过 concat 把它拆开 拼接到 cityData 中
--- js/geo.js
+++ js/geo.js
@@ -10,7 +10,7 @@ const g = svg.append('g').attr('class','root');
 
 //设置地图投影
 const projection = d3.geo.mercator()
-        .center([103, 36])//中心点 兰州
+        .center([105, 37.5])//中心点 兰州
         .scale(800)//放大系数
         .translate([viewWidth/2, viewHeight/2]);
 
@@ -20,14 +20,14 @@ const path = d3.geo.path()
 const color = d3.scale.category20c();
 
 //设置提示框，在鼠标移入的时候显示
-d3.select('#app').append('div')
-  .attr('class', 'tooltip')
-  .attr('opacity', 0.0);
+// d3.select('#app').append('div')
+//   .attr('class', 'tooltip')
+//   .attr('opacity', 0.0);
 //鼠标点击 城市的时候显示
 d3.select('#app').append('div')
   .attr('class', 'cityInfo');
 //请求中国地图数据
-d3.json("/js/china.json", function(error, root){
+d3.json("/data/china_m.json", function(error, root){
 
   if(error){
     return console.error(error);
@@ -35,114 +35,138 @@ d3.json("/js/china.json", function(error, root){
   //地图数据
   const geoData = root.features;
   var cityData = [];
-  //数组降维
+  //数组降维 获得 cityData
   geoData.forEach(val =>{
     if(!val.properties.cities) return;
     cityData = cityData.concat(val.properties.cities);
     return cityData;
   });
   //拖拽
-  var startX = 0,
-      startY = 0;
+  dragMap();
+  //渲染省份的路径
+  renderPath(geoData);
+  //渲染省份名称
+  renderProv(geoData);
+  //渲染城市
+  renderCity(cityData);
+
+function dragMap(){
+ var startX = 0,
+     startY = 0,
+     x = 0,
+     y = 0;
+
   const dragListener = d3.behavior.drag()
         .on('dragstart', function(d,i){
           d3.event.sourceEvent.stopPropagation();
-          startX = d3.event.sourceEvent.clientX;
-          startY = d3.event.sourceEvent.clientY;
+          console.log('dratStart '+ d3.event.sourceEvent, d3.event.sourceEvent);
         })
         .on('drag', dragmove);
+
   function dragmove(d,i){
-    const x = d3.event.x - startX,
-          y = d3.event.y - startY;
-    console.log(d3.event);
+    x = d3.event.x - startX,
+    y = d3.event.y - startY;
+
     d3.select(this)
       .attr("transform",function(d,i){
         return `translate(${x}, ${y})`;
       });
   }
   //最外层 g 添加 属性
-  const translateStr = "translate(0,30)";
-  g.attr('transform',translateStr)
-   .call(dragListener);
-
-  const nodeEnter = g.selectAll("path")
-    .data(geoData)
-    .enter()
-    .append('g')
-    .attr('class', function(d){
-      return d.properties.ename;
-    });
-
-  nodeEnter.append('path')
-    .attr('stroke', "#fff")
-    .attr('stroke-width', 1)
-    .attr("fill", function(d, i){
-      return "lightsteelblue";//color(i);
+  g.attr('transform', "translate(0,0)")
+    .on('mousedown', function(){
+      startX = d3.mouse(this)[0];
+      startY = d3.mouse(this)[1];
+      console.log('mouseDown '+startX, startY);
     })
-    .attr('d', function(d){
-      return path(d);
-    })//鼠标移入显示 提示省份名称
-    .on('mouseenter', function(d, i){
-      d3.select(this)
-        .attr('fill',"yellow");
-      var html = `<div>
-                     <span>${d.properties.name}</span>
-                  </div>`;
-      d3.select('.tooltip')
-        .style('left',(d3.event.pageX)+"px")
-        .style('top',(d3.event.pageY)+"px")
-        .style('opacity',1.0)
-        .html(html);
-    })
-    .on('mouseleave', function(d, i){
-      d3.select(this)
-        .attr('fill', color(i));
-      d3.select('.tooltip')
-        .style('opacity',0.0);
-    });
-    // .on('click', function(d, i){
-    //   console.log(d.properties.ename);
-    //   var x, y, k, centered;
+    .call(dragListener);
+}
 
-    //     if (d && centered !== d) {
-    //         var centroid = path.centroid(d);
-    //         x = centroid[0];
-    //         y = centroid[1];
-    //         k = 2;
-    //         centered = d;
-    //     } else {
-    //         x = viewWidth / 2;
-    //         y = viewHeight / 2;
-    //         k = 1;
-    //         centered = null;
-    //     }
-
-    //     g.transition()
-    //      .duration(750)
-    //      .attr("transform", "translate(" + viewWidth / 2 + "," + viewHeight / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
-    //     .style("stroke-width", 1.5 / k + "px");
-
-    //   ajaxCities(d.properties.ename, x, y, k);
-
-    //   d3.select("#provinces")
-    //     .style('display', "none");
-    // });
-
-  //省份文字显示
-  const prov = svg.append('g')
-        .attr('id','provinces')
-        .attr('transform',translateStr);
+  //渲染城市名称 包括圆圈 和 城市文字 以及点击事件
+  // cityData 城市数据
+  // x y k
+function renderCity(cityData, x, y, k){
+  //清画布
+  d3.selectAll('#cities').remove();
   //城市显示
-  const cities = svg.append('g')
-        .attr('id', 'cities')
-        .attr('transform',translateStr);
-  //省份名称
+  const cities = g.append('g')
+        .attr('id', 'cities');
+  //城市名称之前的圆圈
+  let circles = cities.selectAll("circle")
+      .data(cityData)
+      .enter()
+      .append('circle');
+  //城市名称
+  let texts = cities.selectAll('text')
+       .data(cityData)
+       .enter()
+       .append('text');
+
+  circles.attr('cx', function(d){
+      //根据城市的经纬度投射确定圆点坐标
+      return projection([d.properties.cp[0],d.properties.cp[1]])[0];
+    })
+    .attr('cy', function(d){
+      return projection([d.properties.cp[0],d.properties.cp[1]])[1];
+    })
+    .attr('r', 5)
+    .attr('fill', 'red')
+    .on('click', function(d){
+      clickShow(d);
+    });
+
+  texts.text(function(d){
+      return d.properties.name;
+    })
+    .attr('x', function(d){
+      //根据城市的经纬度投射确定坐标
+      return projection([d.properties.cp[0],d.properties.cp[1]])[0] + 12;
+    })
+    .attr('y', function(d){
+      return projection([d.properties.cp[0],d.properties.cp[1]])[1] + 2;
+    })
+    .attr('fill', "#000")
+    .attr('font-size', '9px');
+
+  //点击关闭 城市信息框
+  $('.cityInfo').on("click",".close",function(){
+    $('.cityInfo').removeClass('active');
+  });
+
+}
+//点击城市 圆圈 显示 城市的具体信息
+function clickShow(d){
+      //console.log(d);
+   var html = ` <i class="close">×</i>
+                  <p>
+                     <span>城市：</span>
+                     <span>${d.properties.name}</span>
+                  </p>
+                  <p>
+                     <span>下属区县：</span>
+                     <span>${d.properties.childNum}</span>
+                  </p>`;
+   d3.select('.cityInfo')
+     .classed('active',true)
+     .style('left',(d3.event.pageX)+"px")
+     .style('top',(d3.event.pageY)+"px")
+     .html(html);
+}
+
+function renderProv(geoData){
+  //清画布
+  d3.selectAll('#provinces').remove();
+ //省份文字显示
+  const prov = g.append('g')
+        .attr('id','provinces');
+
   prov.selectAll('text')
     .data(geoData)
     .enter()
     .append('text')
     .text(function(d){
       let proName = d.properties.name;
+      //香港 澳门 太挤了 不显示
       if(proName === "澳门" || proName === "香港"){
         proName = '';
       }
@@ -158,102 +182,90 @@ d3.json("/js/china.json", function(error, root){
     .attr('fill', "#000")
     .attr('font-size', '14px')
     .attr('font-weight', 'bold');
-  //城市名称之前的圆圈
-  cities.selectAll("circle")
-    .data(cityData)
+}
+
+function ajaxCities(city, x, y, k, cityData){
+  // d3.json(`/data/${city}.json`,function(error, root){
+  //   //console.log(root.features);
+  //   let data = root.features,
+  //       cities = d3.select('#cities');
+  //   d3.selectAll('#cities > *').remove();
+  //   renderCity(data, x, y, k);
+  // });
+  renderCity(cityData, x, y, k);
+}
+
+function renderPath(geoData){
+  //鼠标点击时放大的中心点
+  let centered = null;
+
+  const nodeEnter = g.selectAll("path")
+    .data(geoData)
     .enter()
-    .append('circle')
-    .attr('cx', function(d){
-      //根据城市的经纬度投射确定圆点坐标
-      return projection([d.properties.cp[0],d.properties.cp[1]])[0];
-    })
-    .attr('cy', function(d){
-      return projection([d.properties.cp[0],d.properties.cp[1]])[1];
-    })
-    .attr('r', 5)
-    .attr('fill', 'red')
-    .on('click', function(d){
-      //console.log(d);
-      var html = ` <i class="close">×</i>
-                   <p>
-                       <span>城市：</span>
-                       <span>${d.properties.name}</span>
-                     </p>
-                     <p>
-                       <span>下属区县：</span>
-                       <span>${d.properties.childNum}</span>
-                  </p>`;
-      d3.select('.cityInfo')
-        .classed('active',true)
-        .style('left',(d3.event.pageX)+"px")
-        .style('top',(d3.event.pageY)+"px")
-        .html(html);
+    .append('g')
+    .attr('class', function(d){
+      return d.properties.ename;
     });
-  //点击关闭 城市信息框
-  $('.cityInfo').on("click",".close",function(){
-    $('.cityInfo').removeClass('active');
-  });
-  //城市名称
-  cities.selectAll('text')
-    .data(cityData)
-    .enter()
-    .append('text')
-    .text(function(d){
-      return d.properties.name;
+  nodeEnter.append('path')
+    .attr('stroke', "#fff")
+    .attr('stroke-width', 1)
+    .attr("fill", function(d, i){
+      return "lightsteelblue";//color(i);
     })
-    .attr('x', function(d){
-      //根据城市的经纬度投射确定坐标
-      return projection([d.properties.cp[0],d.properties.cp[1]])[0] + 12;
-    })
-    .attr('y', function(d){
-      return projection([d.properties.cp[0],d.properties.cp[1]])[1] + 2;
-    })
-    .attr('fill', "#000")
-    .attr('font-size', '9px');
+    .attr('d', function(d){
+      return path(d);
+    })//鼠标移入显示 提示省份名称
+    // .on('mouseover', function(d, i){
+    //   var html = `<div>
+    //                  <span>${d.properties.name}</span>
+    //               </div>`;
+    //   d3.select('.tooltip')
+    //     .style('left',(d3.event.pageX)+"px")
+    //     .style('top',(d3.event.pageY)+"px")
+    //     .style('opacity',1.0)
+    //     .html(html);
+    // })
+    // .on('mouseleave', function(d, i){
+    //   d3.select(this)
+    //     .attr('fill', color(i));
+    //   d3.select('.tooltip')
+    //     .style('opacity',0.0);
+    // })//点击该省份放大
+    .on('dbclick', function(d, i){
+      //console.log(d.properties.ename);
+      //颜色改变
+      d3.select(this)
+        .attr('fill', color(i));
 
+        var x, y, k, ename = d.properties.ename;
+        //如果当前不是中心点放大， 如果是 缩小 k 为 放大缩小的倍数
+        if (d && centered !== d) {
+          let centroid = path.centroid(d);
+            x = centroid[0];
+            y = centroid[1];
+            k = 2;
+            centered = d;
+           // d3.select("#provinces")
+           //   .style('display', "none");
+
+          //ename 是 省份名称 根据这个请求数据
+          ajaxCities(ename, x, y, k,cityData);
+        } else {
+            x = viewWidth / 2;
+            y = viewHeight / 2;
+            k = 1;
+            centered = null;
+          // d3.select("#provinces")
+          //   .style('display', "block");
+          renderCity(cityData);
+        }
+
+        g.transition()
+         .duration(750)
+         .attr("transform", "translate(" + viewWidth / 2 + "," + viewHeight / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
+        .attr("stroke-width", 1.5 / k + "px");
+
+    });
+
+  }
 });
-
-// function ajaxCities(city, x, y, k){
-
-// d3.json(`/data/${city}.json`,function(error, root){
-//   //console.log(root.features);
-//   var  shanxiData = root.features;
-
-//   circles.attr("transform", "translate(" + viewWidth / 2 + "," + viewHeight / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")");
-
-//   texts.attr("transform", "translate(" + viewWidth / 2 + "," + viewHeight / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")");
-
-
-//   circles.selectAll("circle")
-//     .data(shanxiData)
-//     .enter()
-//     .append('circle')
-//     .attr('cx', function(d){
-//       return projection([d.properties.cp[0],d.properties.cp[1]])[0];//根据城市的经纬度投射确定圆点坐标
-//     })
-//     .attr('cy', function(d){
-//       return projection([d.properties.cp[0],d.properties.cp[1]])[1];
-//     })
-//     .attr('r', 3)
-//     .attr('fill', '#29ff57');
-
-//   texts.selectAll('text')
-//     .data(shanxiData)
-//     .enter()
-//     .append('text')
-//     .text(function(d){
-//       return d.properties.name;
-//     })
-//     .attr('x', function(d){
-//       return projection([d.properties.cp[0],d.properties.cp[1]])[0];//根据城市的经纬度投射确定坐标
-//     })
-//     .attr('y', function(d){
-//       return projection([d.properties.cp[0],d.properties.cp[1]])[1];
-//     })
-//     .attr('fill', "#000")
-//     .attr('font-size', '9px');
-
-//     shanxiData = null;
-// });
-
-// }
